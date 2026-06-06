6ec3b83d8aa557c4f4188ecff4853c94b8cceacc
Bertrand Awenze <beawe@ulaval.ca>
2026-06-06 19:06:10 -0400

    feat: ajouter le use case pour la requete de continuation

---
+++ src/application/use-cases/requestGameResumeUseCase.ts
@@ -0,0 +1,11 @@
+import { UseCase } from "./useCase.ts";
+
+export class RequestGameResumeUseCase extends UseCase<void, void> {
+  constructor() {
+    super();
+  }
+
+  execute(): void {
+    this.gameServer.resumeGame();
+  }
+}
