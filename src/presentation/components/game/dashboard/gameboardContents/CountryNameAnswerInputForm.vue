4301c7960ba90e47642ec4355f49d2e5e5cae874
Bertrand Awenze <beawe@ulaval.ca>
2026-08-29 21:30:57 -0400

    fix: eliminer la coupure haute lors du zoom

--- src/presentation/components/game/dashboard/gameboardContents/CountryNameAnswerInputForm.vue
+++ src/presentation/components/game/dashboard/gameboardContents/CountryNameAnswerInputForm.vue
@@ -58,7 +58,13 @@
 </script>
 
 <template>
-  <form id="answer-form" autocomplete="off" :aria-hidden="!isActive" @submit.prevent="onSubmit">
+  <form
+    id="answer-form"
+    class="answer-input-form"
+    autocomplete="off"
+    :aria-hidden="!isActive"
+    @submit.prevent="onSubmit"
+  >
     <div id="answer-input-container" class="answer-input-container" @click="focusInput">
       <input
         id="answer-input"
